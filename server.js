b2271638fec32ac342da69d16cc8256c4a8cd0b1
Joseph Young <joseph.young32@yahoo.com>
2020-01-25 11:45:25 -0500

    deploying boilerplate react app to heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const mongoose = require('mongoose');
 
