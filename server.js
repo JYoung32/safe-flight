5a7440147c7bb3cd5d0d0f7ed3b2a33cef82dc5c
Joseph Young <joseph.young32@yahoo.com>
2020-01-25 11:30:05 -0500

    added mongoDB as db

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/project3",
+  {
+    useMongoClient: true
+  }
+);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
