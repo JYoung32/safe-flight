b7a744cc25b97e7da681d279cf28f8ce1e4fd117
P Skolsky <patrickskolskyart@gmail.com>
2020-01-30 19:49:13 -0500

    pushing in correct parent folder, no mas funky shit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import CreateUser from "./components/Login"
 import Navbar from "./components/Navigation";
 import SignIn from "./components/Signin"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faPlane } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faPlane)
+
+
 
 function App() {
   return (
