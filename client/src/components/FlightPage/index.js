74844de9f71e77a2c86f3ced7ca31f7b78a0ff26
P Skolsky <patrickskolskyart@gmail.com>
2020-02-01 13:01:51 -0500

    there is now a header and an icon

--- client/src/components/FlightPage/index.js
+++ client/src/components/FlightPage/index.js
@@ -1,19 +1,39 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 class FlightPage extends React.Component {
     
     state = {
+        origin: "",
+        destination: "",
+        departure: "",
+        return: ""
 
     };
 
     render() {
         return (
-            <div>
-                
+          <div className="contain fluid">
+              <div className="row">
+                 <div className="col-12 m-4">
+                 <h1><FontAwesomeIcon icon="plane" />
+                      Choose a Destination...</h1>
+                        
+                  
+                  
+                        
+                 </div>
+                 <div className= "col-6 mx-auto">
+                <div className="btn btn-primary btn-block ">
+                          Find Flights
+                        </div>
+                    </div>
+                </div>
             </div>
+              
         );
-    };
-};
+      }
+    }
 
 export default FlightPage; 
