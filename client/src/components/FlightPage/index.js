f4b1f0447e7f5b6ebb33cff2bb62ea8fc699d39e
P Skolsky <patrickskolskyart@gmail.com>
2020-02-03 20:40:29 -0500

    restyled jumbotron

--- client/src/components/FlightPage/index.js
+++ client/src/components/FlightPage/index.js
@@ -6,71 +6,77 @@ import API from "../../utils/API";
 
 class FlightPage extends React.Component {
 
-    state = {
-        origin: "",
-        destination: "",
-        departure: "",
-        return: "",
-        flights: []
+    // state = {
+    //     origin: "",
+    //     destination: "",
+    //     departure: "",
+    //     return: "",
+    //     flights: []
 
-     };
-   
-        constructor(props) {
-            super(props);
-            this.state = {value: '',
+    //  };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
             origin: "",
             destination: "",
             departure: "",
-            return: ""};
-          
-            this.handleChange = this.handleChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-          }
-          
-        handleChange(event) {
-            this.setState({
-               origin: event.target.origin });
-            }
-    
-        handleSubmit(event) {
-            // alert('A name was submitted: ' + this.state.value);
-             event.preventDefault();
-              
-            };
+            return: ""
+        };
 
-    auth = () =>{
-        API.token()
-        .then(data =>console.log(data))
-        .catch(err => console.log(err))
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    loadFlights = () =>{  
-        API.getFlights()
-        .then(data => this.setState({flights: data}))
-        .catch(err => console.log(err));     
+    handleChange(event) {
+        this.setState({
+            origin: event.target.origin
+        });
+    }
+
+    handleSubmit(event) {
+        // alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+
     };
-    
-    componentDidMount(){
-        this.auth()
-        this.loadFlights();
-    };
+
+    // auth = () =>{
+    //     API.token()
+    //     .then(data =>console.log(data))
+    //     .catch(err => console.log(err))
+    // }
+
+    // loadFlights = () =>{  
+    //     API.getFlights()
+    //     .then(data => this.setState({flights: data}))
+    //     .catch(err => console.log(err));     
+    // };
+
+    // componentDidMount(){
+    //     this.auth()
+    //     this.loadFlights();
+    // };
 
     render() {
         return (
-            <div className="container mt-3">
-                <Jumbotron>
+            <div className="container">
+                <Jumbotron className="mt-3 text-center">
                     <h1 className="display-3">Search for Flights</h1>
                     <hr className="my-2" />
                     <Form>
-                        <FormGroup>
-                            <Label for="OriginAirport">Origin</Label>
-                            <Input type="text" name="email" id="originAirport" placeholder="Airport you are leaving from" value={this.state.origin} onChange={this.handleChange} />
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="DestinationAirport">Destination</Label>
-                            <Input type="text" name="email" id="destinationAirport" placeholder="Destination you are traveling to" />
-                        </FormGroup>
-                        <DateRangePicker
+                        <div className="row">
+
+                            <FormGroup className="col-md">
+                                <Label for="OriginAirport">Origin</Label>
+                                <Input type="text" name="origin" id="originAirport" placeholder="Airport you are leaving from" value={this.state.origin} onChange={this.handleChange} />
+                            </FormGroup>
+                            <FormGroup className="col-md">
+                                <Label for="DestinationAirport">Destination</Label>
+                                <Input type="text" name="destination" id="destinationAirport" placeholder="Destination you are traveling to" />
+                            </FormGroup>
+                        </div>
+                        <DateRangePicker className="m-3"
                             startDate={this.state.startDate} // momentPropTypes.momentObj or null,
                             startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
                             endDate={this.state.endDate} // momentPropTypes.momentObj or null,
@@ -80,13 +86,13 @@ class FlightPage extends React.Component {
                             onFocusChange={focusedInput => this.setState({ focusedInput })} // PropTypes.func.isRequired,
                         />
                     </Form>
-                    <Button color="primary">Search Flights</Button>
+                    <Button className="m-3" color="primary">Search Flights</Button>
 
                 </Jumbotron>
             </div>
-              
+
         );
-      }
     }
+}
 
 export default FlightPage; 
