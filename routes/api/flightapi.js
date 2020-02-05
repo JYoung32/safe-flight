b1bbe6960d27f1bede19329e29a86d30f43c9bd4
P Skolsky <patrickskolskyart@gmail.com>
2020-02-04 19:35:46 -0500

    edited coded to match master

--- routes/api/flightapi.js
+++ routes/api/flightapi.js
@@ -1,17 +1,6 @@
 const axios = require("axios");
 const router = require("express").Router();
 
-
-
-
-router.get("/shopping-flightoffers", (req, res) => {
-  axios
-    .get("https://test.api.amadeus.com/v1/shopping/flight-offers?origin=NYC&destination=MAD&departureDate=2020-10-01&max=2")
-    .then(({ data: { results } }) => res.json(results))
-    .catch(err => res.status(422).json(err));
-});
-
-
 router.get('/api/flight')
   .then(console.log("hello router"));
     //data => data);
@@ -24,31 +13,3 @@ router.post('/api/flight', (req,res) => {
   });
 });
 module.exports = router;
-
-
-
-// // Test keys | Base URL: test.api.amadeus.com
-// API Key : 4zGJKgRTOOYMnFeXaAIcFLwtW3AQHHS2
-// API Secret : jmz0kQKKLM0IMkzw
-
-//break into what and how 
-//write logic to renew token based on token experation(can write in the error portion)
-//^^ put into env file, define api call to get token then write second api call to get flight data
-
-// curl "https://test.api.amadeus.com/v1/security/oauth2/token" \
-//      -H "Content-Type: application/x-www-form-urlencoded" \
-//      -d "grant_type=client_credentials&client_id=4zGJKgRTOOYMnFeXaAIcFLwtW3AQHHS2&client_secret=jmz0kQKKLM0IMkzw"
-
-
-     // curl response 
-    //  {
-    //     "type": "amadeusOAuth2Token",
-    //     "username": "kgiovinazzo42@gmail.com",
-    //     "application_name": "Flight Detector",
-    //     "client_id": "4zGJKgRTOOYMnFeXaAIcFLwtW3AQHHS2",
-    //     "token_type": "Bearer",
-    //     "access_token": "UcAFCMRbn8LqlGlWa60rlbJUxVt2",
-    //     "expires_in": 1799,
-    //     "state": "approved",
-    //     "scope": ""
-    // }
