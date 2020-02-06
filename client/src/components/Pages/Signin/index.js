dc6419f439d3036b590d7db96ac668bb2e88ca92
Joseph Young <joseph.young32@yahoo.com>
2020-02-06 11:24:50 -0500

    refactored sign in page to extend react component and add state

--- client/src/components/Pages/Signin/index.js
+++ client/src/components/Pages/Signin/index.js
@@ -4,44 +4,64 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Card, CardBody, Container, Input, Form, FormGroup, Label } from "reactstrap";
 
+class SignIn extends React.Component {
 
-function SignIn() {
-    return (
-        <Container>
-            <div className="row mt-5">
-                <div className="col-md-6 m-auto">
-                    <Card>
-                        <CardBody>
-                            <h1 className="text-center mb-3">
-                                <FontAwesomeIcon icon="user" /> Sign In
-                        </h1>
+    state = {
 
+    }
 
-                            <Form action="/users/register" method="POST">
-                                <FormGroup>
-                                    <Label for="username">Username</Label>
-                                    <Input type="username" id="username" name="username" className="form-control" placeholder="jjog"
-                                    //value="" 
-                                    ></Input>
-                                </FormGroup>
-                                <FormGroup>
-                                    <Label for="password">Password</Label>
-                                    <Input type="password" id="password" name="password" className="form-control" placeholder="********"
-                                    //value="" 
-                                    ></Input>
-                                </FormGroup>
-                                <Button type="submit" className="btn btn-primary btn-block mt-5">
-                                    Sign In
-                            </Button>
-                            </Form>
-                            <p className="mx-auto mt-3">Need an account?<a href="/"> Register</a></p>
-                            <p className="mx-auto"> Need help?<a href="#"> Click here</a></p>
-                        </CardBody>
-                    </Card>
+    handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = (event) => {
+
+    };
+
+    render() {
+        return (
+            <Container>
+                <div className="row mt-5">
+                    <div className="col-md-6 m-auto">
+                        <Card>
+                            <CardBody>
+                                <h1 className="text-center mb-3">
+                                    <FontAwesomeIcon icon="user" /> Sign In
+                            </h1>
+    
+    
+                                <Form action="/users/register" method="POST">
+                                    <FormGroup>
+                                        <Label for="username">Username</Label>
+                                        <Input type="username" id="username" name="username" className="form-control" placeholder="jjog"
+                                        //value="" 
+                                        ></Input>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Label for="password">Password</Label>
+                                        <Input type="password" id="password" name="password" className="form-control" placeholder="********"
+                                        //value="" 
+                                        ></Input>
+                                    </FormGroup>
+                                    <Button type="submit" className="btn btn-primary btn-block mt-5">
+                                        Sign In
+                                </Button>
+                                </Form>
+                                <p className="mx-auto mt-3">Need an account?<a href="/"> Register</a></p>
+                                <p className="mx-auto"> Need help?<a href="#"> Click here</a></p>
+                            </CardBody>
+                        </Card>
+                    </div>
                 </div>
-            </div>
-        </Container>
-    );
+            </Container>
+        );
+    }
 }
 
 export default SignIn;
